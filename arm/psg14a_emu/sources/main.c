43acfafb3cac53d5433d59f5a2d2a50ad7a6e98c
Mykhaylo Shcherbak <58140198+mykhayloscherbak@users.noreply.github.com>
2018-12-30 14:40:52 +0000

    [psg14a_emu]First runnable commit

--- arm/psg14a_emu/sources/main.c
+++ arm/psg14a_emu/sources/main.c
@@ -1,8 +1,7 @@
-/*
- * main.c
- *
- *  Created on: Dec 30, 2018
- *      Author: mikl
+/**
+ * @file main.c
+ * @e mikl74@yahoo.com
+ * @brief Main file for psg14a_emu - project for psg14a starter-alternator control panel for GTD350 engine
  */
 
 void main(void)
