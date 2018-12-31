de6d96560ec24179092c2cc8f2392e180223fa84
Mykhaylo Shcherbak <58140198+mykhayloscherbak@users.noreply.github.com>
2018-12-31 16:23:22 +0000

    [psg14a_emu]temp commit

--- arm/psg14a_emu/sources/main.c
+++ arm/psg14a_emu/sources/main.c
@@ -3,12 +3,15 @@
  * @e mikl74@yahoo.com
  * @brief Main file for psg14a_emu - project for psg14a starter-alternator control panel for GTD350 engine
  */
+#include <stdint.h>
 #include "clock_and_timers.h"
+
+volatile uint32_t x[3]={1,2,3};
 void main(void)
 {
-	Clock_HSI_Init();
+    Clock_HSI_Init();
 	while(1)
 	{
-
+		x[1]++;
 	}
 }
