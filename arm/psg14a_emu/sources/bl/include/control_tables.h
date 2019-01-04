532bbfb734f765a9ab0ac6dabe0a87f4a9f7493e
Mykhaylo Shcherbak <58140198+mykhayloscherbak@users.noreply.github.com>
2019-01-04 23:49:53 +0000

    [psg14a_emu]Temp

---
+++ arm/psg14a_emu/sources/bl/include/control_tables.h
@@ -0,0 +1,45 @@
+/**
+ * @file control_tables.h
+ * @e mikl74@yahoo.com
+ * @date 05-01-2019
+ * @version 1.00
+ * @author Mykhaylo Shcherbak
+ * @brief Contains prototypes and types for main outputs control table
+ */
+
+#ifndef SOURCES_BL_INCLUDE_CONTROL_TABLES_H_
+#define SOURCES_BL_INCLUDE_CONTROL_TABLES_H_
+
+typedef enum
+{
+	CH_STARTER = 0,
+	CH_PRIMING_FUEL,
+	CH_SPARK,
+	CH_OUT3,
+	CH_OUT4,
+	CH_OUT5,
+	CH_OUT6,
+	CH_OUT7,
+	CH_NO_CH
+} Control_channels_id;
+
+typedef enum
+{
+	OUT_MODE_OFF = 0,
+	OUT_MODE_ON,
+	OUT_MODE_PULSED
+} Out_Mode_t;
+
+/**
+ * @brief Struct for each line in a table. It describes which channel must be in which state after described time
+ */
+typedef struct
+{
+	Control_channels_id Channel; /** Channel id */
+	uint32_t ms;                 /** At which ms make this state active */
+	Out_Mode_t Mode;	         /** Mode which must be active. Modes are on,off or pulsed now */
+	uint32_t On;				 /** Time output is on in pulsed mode */
+	uint32_t Off;				 /** Time output is off in pulsed mode */
+} Channel_Step_t;
+
+#endif /* SOURCES_BL_INCLUDE_CONTROL_TABLES_H_ */
---
+++ arm/psg14a_emu/sources/bl/src/control_tables.c
@@ -0,0 +1,17 @@
+/**
+ * @file control_tables.c
+ * @e mikl74@yahoo.com
+ * @author Mykhaylo Shcherbak
+ * @date 05-01-2019
+ * @version 1.00
+ * @brief Contains main control table
+ */
+
+#include "control_tables.h"
+
+static Channel_Step_t Cyclogram_Cold[]=
+{
+		{.Channel = CH_STARTER,.Mode = OUT_MODE_ON,.ms = 0, .On = 0,.Off = 0},
+		{.Channel = CH_STARTER,.Mode = OUT_MODE_OFF, .ms = 1000, .On = 0, .Off = 0},
+		{.Channel = CH_NO_CH}
+};
